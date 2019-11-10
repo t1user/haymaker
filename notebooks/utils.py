06847c7d13b7d79c63e7774cce026c8843100fc0
t1user <tomasz2605@gmail.com>
2019-11-10 22:41:12 +0100

    utils bug fixes

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -33,7 +33,7 @@ def plot(*data):
 
 def chart_price(price_series, signal_series, long_threshold=0, short_threshold=0):
     """
-    Plot a price chart marking where long and short positions would be
+    Plot a price chart marking where long and short positions would be,
     given values of signal.
     price_series: instrument prices
     signal_series: indicator based on which signals will be generated
@@ -55,15 +55,20 @@ def backtest(price: pd.Series, indicator: pd.Series,
     Run a quick and dirty backtest for a system that goes long
     when indicator is above threshold and short when indicator
     is below minus threshold. Commissions and spreads are not accounted for.
-    Transactions are assumed to be executed on the next price point after signal.
+    Transactions are assumed to be executed on the next price point after
+    trade signal is generated. Positions are marked to market.
 
     Args:
-    price: Series with instrument prices, must be indexed with Timestamps
-    indicator: Series with indicator values moving around zero, same index as price
-    threshold: above threshold strategy goes long below minus threshold short
-    multiplier: multiplier of the futures contract that corresponds to price
-    bankroll: bankroll to be used for daily returns assuming trading with one 
-              contract
+        price:       Series with instrument prices,
+                     must be indexed with Timestamps
+        indicator:   Series with indicator values moving around zero,
+                     same index as price
+        threshold:   strategy goes long when indicator above threshold,
+                     short when price below minus threshold short
+        multiplier:  multiplier for the futures contract that corresponds
+                     to price
+        bankroll:    bankroll to be used for daily returns assuming
+                     trading with one contract
     """
     # Main simulation DataFrame
     df = pd.DataFrame({'price': price,
@@ -92,31 +97,40 @@ def backtest(price: pd.Series, indicator: pd.Series,
     pyfolio_stats = perf_stats(daily['returns'])
 
     # DataFrame calculating per transaction stats
-    transactions = df[df.signal != df.signal.shift()].iloc[1:][[
-        'signal', 'cum_pnl_dollars']]
-    transactions['transaction_pnl'] = transactions['cum_pnl_dollars'].diff()
-    win_trans = transactions[transactions.transaction_pnl > 0]
-    los_trans = transactions[transactions.transaction_pnl < 0]
+    transactions = df[df.signal !=
+                      df.signal.shift(-1)][['signal', 'cum_pnl_dollars']]
+    transactions.merge(
+        df[df.signal != df.signal.shift(1)][['signal', 'cum_pnl_dollars']], how='outer')
+    transactions.reset_index(inplace=True)
+    positions = transactions[transactions.index % 2 == 0]
+    last_row = transactions[transactions.index % 2 != 0].iloc[-1]
+    if last_row.date != positions.date.iloc[-1]:
+        positions = positions.append(last_row)
+    positions['transaction_pnl'] = positions['cum_pnl_dollars'].diff()
+    positions = positions.iloc[1:]
+    positions.set_index('date', inplace=True)
+    win_trans = positions[positions.transaction_pnl > 0]
+    loss_trans = positions[positions.transaction_pnl < 0]
 
     # container for all non-pyfolio stats
     stats = pd.Series()
-    stats['Win percent'] = (len(win_trans)
-                            / (len(win_trans) + len(los_trans)))
-    stats['Average gain'] = (win_trans.transaction_pnl.sum()
-                             / len(win_trans))
-    stats['Average loss'] = (los_trans.transaction_pnl.sum()
-                             / len(los_trans))
-    stats['Avg Win/avg loss ratio'] = abs(
-        stats['Average gain'] / stats['Average loss'])
-    stats['Transaction EV'] = ((stats['Win percent'] * stats['Average gain'])
-                               + ((1 - stats['Win percent']) * stats['Average loss']))
-    stats['Number of transactions'] = len(win_trans) + len(los_trans)
-
-    trades = df[df.signal != df.signal.shift()].signal.count()
+    positions = positions.signal.count()
+    stats['Win percent'] = len(win_trans) / positions
+    stats['Average gain'] = win_trans.transaction_pnl.sum() / len(win_trans)
+    # positions with zero gain are loss making
+    stats['Average loss'] = (loss_trans.transaction_pnl.sum()
+                             / (positions - len(win_trans)))
+    stats['Avg gain/loss ratio'] = abs(stats['Average gain'] /
+                                       stats['Average loss'])
+    stats['Position EV'] = ((stats['Win percent'] * stats['Average gain'])
+                            + ((1 - stats['Win percent']) * stats['Average loss']))
     days = daily.returns.count()
-    stats['Per day trades'] = trades/days
-    stats['Per trade days'] = days/trades
-    print(pyfolio_stats)
+    stats['Positions per day'] = positions/days
+    stats['Days per position'] = days/positions
+    stats['Days'] = days
+    stats['Positions'] = positions
+    stats['Trades'] = transactions.signal.count() - 1
+    print(pyfolio_stats.to_string())
     print()
-    print(stats)
+    print(stats.to_string())
     daily.balance.plot(figsize=(20, 10), grid=True)
