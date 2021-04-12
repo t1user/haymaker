8fb7446f34239e193906f6668d777945e536b075
t1user <tomasz2605@gmail.com>
2021-04-12 19:08:15 +0200

    run live updated with latest

--- run_live.py
+++ run_live.py
@@ -4,17 +4,15 @@ from dataclasses import dataclass
 
 from ib_insync import IB, util, ContFuture
 from ib_insync.ibcontroller import IBC, Watchdog
-from logbook import DEBUG
 
 from handlers import Handlers
 from saver import ArcticSaver
 from blotter import MongoBlotter
 from manager import Manager
-from trader import Trader
 from streamers import VolumeStreamer
-from candle import BreakoutCandle
-from portfolio import WeightedAdjustedPortfolio
-from execution_models import EventDrivenExecModel
+from candle import BreakoutLockCandleVolFilter, BreakoutCandleVolFilter
+from portfolio import AdjustedPortfolio
+from execution_models import EventDrivenTakeProfitExecModel
 from logger import rotating_logger_with_shell
 
 
@@ -34,73 +32,86 @@ class Params:
     ema_fast: int = 5  # number of periods for moving average filter
     ema_slow: int = 120  # number of periods for moving average filter
     sl_atr: int = 1  # stop loss in ATRs
-    atr_periods: int = 180  # number of periods to calculate ATR on
+    atr_periods: int = 50  # number of periods to calculate ATR on
     trades_per_day: int = 0
     alloc: float = 0  # fraction of capital to be allocated to instrument
     # candle volume to be calculated as average of x periods
     avg_periods: Optional[int] = None
     volume: Optional[int] = None  # candle volume given directly
     min_atr: float = 0  # minimum atr threshold used for for calculations
+    lock_periods_multiple: float = 2
+    tp_multiple: float = 2
+    lock_filter: float = 0.005
+
+    def __post_init__(self) -> None:
+        self.lock_periods = int(self.periods / self.lock_periods_multiple)
 
 
 nq = Params(
     contract=ContFuture('NQ', 'GLOBEX'),
     micro_contract=ContFuture('MNQ', 'GLOBEX'),
     trades_per_day=4.5,
-    # avg_periods=60,
     volume=12000,
     min_atr=14,
-    alloc=.4,
+    lock_filter=0.004,
+    tp_multiple=8,
 )
 
 es = Params(
     contract=ContFuture('ES', 'GLOBEX'),
     micro_contract=ContFuture('MES', 'GLOBEX'),
-    trades_per_day=1.5,
+    trades_per_day=.5,
     ema_fast=120,
     ema_slow=320,
+    atr_periods=180,
     sl_atr=3,
-    # avg_periods=60,
     volume=43000,
     min_atr=5,
-    alloc=.125,
+    lock_periods_multiple=1,
+    tp_multiple=2,
 )
 
 gc = Params(
     contract=ContFuture('GC', 'NYMEX'),
     micro_contract=ContFuture('MGC', 'NYMEX'),
-    trades_per_day=2.5,
+    trades_per_day=1.0,
     ema_fast=60,
     periods=60,
     sl_atr=2,
-    atr_periods=90,
-    # avg_periods=60,
     volume=5500,
     min_atr=1.9,
-    alloc=.225,
+    tp_multiple=6,
 )
 
 ym = Params(
     contract=ContFuture('YM', 'ECBOT'),
     micro_contract=ContFuture('MYM', 'ECBOT'),
-    trades_per_day=1.5,
+    trades_per_day=1,
     ema_fast=60,
     sl_atr=2,
-    # avg_periods=60,
     volume=8000,
+    tp_multiple=6,
     min_atr=55,
-    alloc=.25
 )
 
 
-contracts = [nq, es, ym, gc]
-candles = [BreakoutCandle(VolumeStreamer(params.volume,
-                                         params.avg_periods),
-                          contract_fields=['contract', 'micro_contract'],
-                          **params.__dict__)
-           for params in contracts]
-portfolio = WeightedAdjustedPortfolio(target_vol=.5)
-exec_model = EventDrivenExecModel()
+exec_model = EventDrivenTakeProfitExecModel()
+
+candles = [BreakoutLockCandleVolFilter(VolumeStreamer(params.volume,
+                                                      params.avg_periods),
+                                       contract_fields=[
+                                           'contract', 'micro_contract'],
+                                       **params.__dict__)
+           for params in [es]]
+
+candles.extend([BreakoutCandleVolFilter(VolumeStreamer(params.volume,
+                                                       params.avg_periods),
+                                        contract_fields=[
+    'contract', 'micro_contract'],
+    **params.__dict__)
+    for params in [nq, ym]])
+
+portfolio = AdjustedPortfolio(target_vol=.55)
 
 
 class Start(Handlers):
