2d415a4bb6eb370343e95b1206c34125a9ca54d2
t1user <tomasz2605@gmail.com>
2021-05-02 19:41:53 +0200

    research and tweaks

--- run_backtest.py
+++ run_backtest.py
@@ -13,18 +13,18 @@ from test_strategy import strategy_kwargs
 
 log = logger(__file__[:-3], WARNING, WARNING)
 
-start_date = '20180701'
-end_date = '20191231'
+start_date = '20200101'
+end_date = '20201231'
 cash = 120000
 store = ArcticStore('TRADES_30_secs')
 source = DataSourceManager(store, start_date, end_date)
-ib = IB(source, mode='db_only', index=-4)  # mode is: 'db_only' or 'use_ib'
+ib = IB(source, mode='db_only', index=-1)  # mode is: 'db_only' or 'use_ib'
 
 util.logToConsole()
 asyncio.get_event_loop().set_debug(True)
 
 blotter = CsvBlotter(save_to_file=False, filename='backtest', path='backtests',
-                     note=f'_{start_date}_{end_date}_base_fixed')
+                     note=f'_{start_date}_{end_date}_fixed_NQ_13')
 saver = PickleSaver('notebooks/freeze/backtest')
 manager = Manager(ib, saver=saver, blotter=blotter, **strategy_kwargs)
 market = Market(cash, manager, reboot=False)
