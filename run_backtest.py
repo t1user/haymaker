be804b67bd8e065a3926ee9d6ba81cf366085cbb
t1user <tomasz2605@gmail.com>
2022-11-09 13:06:11 +0100

    blacked

--- run_backtest.py
+++ run_backtest.py
@@ -13,20 +13,23 @@ from test_strategy import strategy_kwargs
 
 log = logger(__file__[:-3], WARNING, WARNING)
 
-start_date = '20210101'
-end_date = '20210921'
+start_date = "20220201"
+end_date = "20220630"
 cash = 80000
-store = ArcticStore('TRADES_30_secs')
+store = ArcticStore("TRADES_30_secs")
 source = DataSourceManager(store, start_date, end_date)
-ib = IB(source, mode='db_only', index=-1)  # mode is: 'db_only' or 'use_ib'
+ib = IB(source, mode="db_only", index=-1)  # mode is: 'db_only' or 'use_ib'
 
 util.logToConsole()
 asyncio.get_event_loop().set_debug(True)
 
-blotter = CsvBlotter(save_to_file=False, filename='backtest',
-                     path='/home/tomek/ib_data/backtests',
-                     note=f'_{start_date}_{end_date}_fixed')
-saver = PickleSaver('/home/tomek/ib_data/freeze/backtest')
+blotter = CsvBlotter(
+    save_to_file=False,
+    filename="backtest",
+    path="/home/tomek/ib_data/backtests",
+    note=f"_{start_date}_{end_date}_fixed",
+)
+saver = PickleSaver("/home/tomek/ib_data/freeze/backtest")
 manager = Manager(ib, saver=saver, blotter=blotter, **strategy_kwargs)
 market = Market(cash, manager, reboot=False)
 ib.run()
