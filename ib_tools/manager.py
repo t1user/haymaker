95d05715e104393c71e2e2edbfe63eecab95c148
t1user <tomasz2605@gmail.com>
2023-10-18 11:21:10 +0200

    logging moved to separate module

--- ib_tools/manager.py
+++ ib_tools/manager.py
@@ -3,13 +3,15 @@ from __future__ import annotations
 import asyncio
 import logging
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Final
 
 import ib_insync as ibi
 
+from ib_tools import misc
 from ib_tools.base import CONTRACT_LIST, Atom, Pipe
 from ib_tools.controller import Controller
-from ib_tools.queue_logger import setup_logging_queue
+from ib_tools.logging import create_task, setup_logging_queue
 from ib_tools.runner import App
 from ib_tools.state_machine import StateMachine
 from ib_tools.streamers import Streamer
@@ -18,7 +20,7 @@ log = logging.getLogger(__name__)
 
 print("About to setup logger...")
 logging.basicConfig(
-    format="[%(asctime)s] %(levelname)8s: %(name)20s | %(message)s"
+    format="%(asctime)s | %(levelname)-8s: | %(name)20s | %(message)s"
     " | %(module)s %(funcName)s %(lineno)d",
     level=5,
 )
@@ -29,7 +31,7 @@ logging.addLevelName(60, "NOTIFY")
 
 # shut up foreign loggers
 logging.getLogger("ib_insync").setLevel(logging.ERROR)
-logging.getLogger("asyncio").setLevel(logging.DEBUG)
+logging.getLogger("asyncio").setLevel(logging.INFO)
 logging.getLogger("numba").setLevel(logging.CRITICAL)
 
 setup_logging_queue()
@@ -39,6 +41,7 @@ Atom.set_ib(IB)
 STATE_MACHINE: Final[StateMachine] = StateMachine()
 CONTROLLER: Final[Controller] = Controller(STATE_MACHINE, IB)
 CONTRACT_DETAILS: dict[ibi.Contract, ibi.ContractDetails] = {}
+TRADING_HOURS: dict[ibi.Contract, list[tuple[datetime, datetime]]]
 
 
 @dataclass
@@ -62,14 +65,30 @@ def acquire_contract_details():
             raise
 
 
+def set_trading_hours() -> None:
+    global TRADING_HOURS
+    TRADING_HOURS = dict()
+    for contract, details in CONTRACT_DETAILS.items():
+        TRADING_HOURS[contract] = misc.process_trading_hours(details.tradingHours)
+
+
 async def prepare():
     await IB.qualifyContractsAsync(*CONTRACT_LIST)
     log.debug(f"contracts qualified {set([c.symbol for c in CONTRACT_LIST])}")
     acquire_contract_details()
     # log.debug(f"Details acquired: {list(CONTRACT_DETAILS.keys())}")
     log.debug(f"Details aquired: {set([k.symbol for k in CONTRACT_DETAILS.keys()])}")
+    set_trading_hours()
+    Atom.trading_hours = TRADING_HOURS
+    log.debug(f"Trading hours: {Atom.trading_hours}")
 
-    await asyncio.gather(*Streamer.awaitables(), return_exceptions=True)
+    await asyncio.gather(
+        *[
+            create_task(s, logger=log, message="asyncio error")
+            for s in Streamer.awaitables()
+        ],
+        return_exceptions=True,
+    )
 
 
 log.debug("Will instantiate the App...")
